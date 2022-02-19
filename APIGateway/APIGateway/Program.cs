72f2b5f7c5ab98635bd05f3d6f9f3e7dec071888
Stefan Radojević <radojevic.stefan.sr@gmail.com>
2022-02-19 21:59:14 +0100

    Added initial API Gateway

--- APIGateway/APIGateway/Program.cs
+++ APIGateway/APIGateway/Program.cs
@@ -1,26 +1,70 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace APIGateway
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Ocelot.Middleware;
+    using Ocelot.DependencyInjection;
+    using System;
+    using System.IO;
+    using Commons.ExceptionHandling;
+    using APIGateway.Middlewares;
+    using Microsoft.Net.Http.Headers;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder().Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IHostBuilder CreateHostBuilder() =>
+            (IHostBuilder) new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("ocelot.json")
+                .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s =>
+            {
+                s.AddOcelot();
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                // TODO: add logging
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                var config = new OcelotPipelineConfiguration
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    PreErrorResponderMiddleware = async (context, next) =>
+                    {
+                        try
+                        {
+                            await next.Invoke();
+                        } catch (BaseException ex)
+                        {
+                            await APIGatewayExceptionHandler.HandleExceptionAsync(context, ex);
+                        }
+
+                        await next.Invoke();
+                    },
+                    PreAuthenticationMiddleware = async (context, next) =>
+                    {
+                        TokenValidationMiddleware.ValidateToken(context?.Request?.Headers[HeaderNames.Authorization]);
+
+                        await next.Invoke();
+                    }
+                };
+
+                app.UseOcelot(config).Wait();
+            });
     }
 }
---
+++ Common/Common/Common/ExceptionHandling/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Commons.ExceptionHandling
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+}
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (BaseException ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+}
+
+        private static Task HandleExceptionAsync(HttpContext context, BaseException ex)
+        {
+            if (ex.code == 0)
+            {
+                ex.code = HttpStatusCode.InternalServerError; 
+            }
+
+            var result = JsonConvert.SerializeObject(new
+            {
+                messsage = ex.Message,
+                status = ex.code,
+                requested_uri = context.Request.Path,
+                origin = ex.origin,
+                timestamp = DateTime.Now
+            });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)ex.code;
+
+            return context.Response.WriteAsync(result);
+        }
+
+    }
+}
