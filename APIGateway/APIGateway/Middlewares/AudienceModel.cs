72061f10c18e5bd428796ab08a5b2756f233a83b
Stefan Radojević <57040379+Stefi99R@users.noreply.github.com>
2022-02-23 21:08:27 +0100

    Build fix (#18)
    * Complaint initial
    
    * ComplaintType with database
    
    * ComplaintStatus, ComplaintEvent, Complaint
    
    * Complaint with database
    
    * Fixed GetById
    
    * Ad with database
    
    * Put (Update)
    
    * Put (Update)
    
    * Added initial API Gateway
    
    * Swagger
    
    * Swagger
    
    * Swagger fixed
    
    * Routes fixed
    
    * Routes fixed
    
    * Put (return fixed)
    
    * Put (fixed return)
    
    * Logger
    
    * Logger
    
    * async
    
    * async
    
    * Minor formating
    
    * Communication with PublicBidding
    
    * Communication with Public Bidding
    
    * Communication with Buyer
    
    * Migration fixed
    
    * Migration fixed
    
    * Added routes
    
    * Added routes
    
    * Microservice/plot (#3)
    
    * Entities created.
    
    * API created for resource PlotCadastralMunicipality
    
    * API created for resource PlotCulture
    
    * API created for resource PlotWorkability
    
    * API created for resource PlotPartFormOfOwnership
    
    * API created for resource PlotPartClass
    
    * API created for resource PlotPartProtectedZone
    
    * API created for resources PlotPart and Plot
    
    * Fluent Validation library implemented for validating user input
    
    * REST naming convention for resources API-s added
    
    * Swagger documentation added
    
    * Logger service added (implemented)
    
    * Asyncronous communication implemented
    
    * Plot microservice communication with Logger service implemented
    
    * Communication between Plot microservice and Buyer microservice mocked
    
    * Plot microservice and Buyer microservice mocked communication revised
    
    * App port changed
    
    Co-authored-by: Andrija Pavlov <pavlovandrija9@gmail.com>
    
    * Microservice/auction (#4)
    
    * CRUD methods on class DocumentationIndividual done
    
    * API for microservice fixed
    
    * Fluent validation for API added
    
    * Swagger documentation done
    
    * Asynchronous communication implemented
    
    * API, mocks, validators done for Auction and Registration microservice
    
    * communication between microservices implemented
    
    * Logger service added in class ServiceCall
    
    * ports added
    
    * Added token validation
    
    Co-authored-by: Mihajlo Strajin <strajin.mihajlo@uns.ac.rs>
    Co-authored-by: Andrija Pavlov <pavlovandrija9@gmail.com>
    Co-authored-by: PanicLuka <56599686+PanicLuka@users.noreply.github.com>

---
+++ APIGateway/APIGateway/Middlewares/AudienceModel.cs
@@ -0,0 +1,23 @@
+﻿namespace APIGateway.Middlewares
+{
+    /// <summary>
+    /// Audience model.
+    /// </summary>
+    public class AudienceModel
+    {
+        /// <summary>
+        /// Secret.
+        /// </summary>
+        public string Secret { get; set; }
+
+        /// <summary>
+        /// Issuer.
+        /// </summary>
+        public string Iss { get; set; }
+
+        /// <summary>
+        /// Audience.
+        /// </summary>
+        public string Aud { get; set; }
+    }
+}
