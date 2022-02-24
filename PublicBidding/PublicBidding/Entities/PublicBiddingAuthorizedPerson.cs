0c368c79f58d1c8ed20650156d1a73f4b3b15ea7
DavorJelic <56700850+DavorJelic@users.noreply.github.com>
2022-02-24 01:07:50 +0100

    Davor jelic (#21)
    * Fixed sonar issues
    
    * Fixed sonar issues

--- PublicBidding/PublicBidding/Entities/PublicBiddingAuthorizedPerson.cs
+++ PublicBidding/PublicBidding/Entities/PublicBiddingAuthorizedPerson.cs
@@ -8,7 +8,7 @@ namespace PublicBidding.Entities
     public class PublicBiddingAuthorizedPerson
     {
         public Guid PublicBiddingId { get; set; }
-        public PublicBidding PublicBidding { get; set; }
+        public PublicBidding? PublicBidding { get; set; }
         public Guid AuthorizedPersonId { get; set; }
     }
 }
